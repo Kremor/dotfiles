2d280235e47478b3512fe78956d5122399ced5bb
Kremor Pärt <kremor.part@gmail.com>
2019-01-15 22:09:16 -0600

    added more dot commands

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,9 @@
 # Adds ~/bin to PATH
-set PATH $PATH /home/kremor/bin/ /home/kremor/.local/bin/
+set PATH $PATH $HOME/.local/bin/
 
 
 # git but for dot files
 alias dot='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-
+alias dot-commit='dot commit -am'
+alias dot-ls='dot ls-tree -r HEAD --name-only'
 alias dot-remove='dot rm --cached'
