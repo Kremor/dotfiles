cc4bd61590bcfd2670ca908cba1e6a21fd6cba70
Kremor Pärt <kremor.part@gmail.com>
2019-01-24 11:40:42 -0600

    Adds elpy to emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,6 +13,12 @@
 (require 'whitespace)
 (global-whitespace-mode)
 
+;; Delete trailing whitespaces before saving
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; Adds a new line to the end of the file
+(setq require-final-newline t)
+
 ;; Melpa
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -24,11 +30,6 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-;; Srcery Theme
-(use-package srcery-theme
-	     :ensure t
-	     :config
-	     (load-theme 'srcery t))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -37,10 +38,30 @@
  '(custom-safe-themes
    (quote
     ("144f05e2dfa7a7b50cad0c3519498ac064cc9da1f194b8ea27d0fb20129d8d7a" default)))
- '(package-selected-packages (quote (srcery-theme use-package))))
+ '(package-selected-packages
+   (quote
+    (company elpy srcery-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Srcery Theme
+(use-package srcery-theme
+  :ensure t
+  :config
+  (load-theme 'srcery t))
+
+;; Company Mode
+(use-package company
+  :ensure t
+  :config
+  (add-hook 'after-init-hook 'global-company-mode))
+
+;; Elpy
+(use-package elpy
+  :ensure t
+  :config
+  (elpy-enable))
