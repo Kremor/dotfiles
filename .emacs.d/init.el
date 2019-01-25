b33df40b0abcf50bcfd2788801097d406c068378
Kremor Pärt <kremor.part@gmail.com>
2019-01-25 12:42:40 -0600

    Adds setup for JS

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -37,7 +37,7 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+	         '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
@@ -50,11 +50,29 @@
   :config
   (load-theme 'srcery t))
 
+;; Company
+(use-package company
+  :ensure t
+  :config
+  (add-hook 'after-init-hook 'global-company-mode))
+
 ;; Elpy
 (use-package elpy
   :ensure t
   :config
   (elpy-enable))
+
+;; JS Mode
+(use-package js2-mode
+  :ensure t
+  :config
+  (use-package indium
+    :ensure t)
+  (use-package prettier-js
+    :ensure t
+    :config
+    (add-hook 'js2-mode-hook 'prettier-js-mode))
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
 
 ;; Magit
 (use-package magit
@@ -97,7 +115,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (sublimity use-package srcery-theme elpy))))
+ '(package-selected-packages
+   (quote
+    (prettier-js sublimity use-package srcery-theme elpy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
