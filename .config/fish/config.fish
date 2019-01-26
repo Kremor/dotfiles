def143f6030cffd28d530e840069335febfe5861
Kremor Pärt <kremor.part@gmail.com>
2019-01-26 12:57:58 -0600

    Fish uses PyWal to change terminal colors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,3 +7,10 @@ alias dot='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias dot-commit='dot commit -am'
 alias dot-ls='dot ls-tree -r HEAD --name-only'
 alias dot-remove='dot rm --cached'
+
+
+# Loads terminal colors using wal
+set wal_path (whereis wal)
+if not test "$wal-path" = ""
+    wal -n -R -q
+end
