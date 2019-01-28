8448a917abb5e13aa5720e1059d3c6c9919e5f25
Kremor Pärt <kremor.part@gmail.com>
2019-01-28 07:44:04 -0600

    Adds space centric bindings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -47,6 +47,44 @@
  "M-<down>" 'my-move-line-down
  "M-<up>" 'my-move-line-up)
 
+;; Remaps keys to be more space centric
+(general-unbind "C-SPC")
+(general-define-key
+ :prefix "C-x"
+ "SPC" 'set-mark-command)
+(general-define-key
+ :prefix "C-SPC"
+
+ ;; bookmarks
+ "m" '(:ignore t :which-key "bookmarks")
+ "ml" '(bookmark-bmenu-list :which-key "list")
+ "mm" '(bookmark-set :which-key "set")
+
+
+ ;; buffers
+ "b" '(:ignore t :which-key "buffers")
+ "bb" '(ivy-switch-buffer :which-key "switch")
+ "bk" '(kill-this-buffer :which-key "kill")
+ "bl" '(list-buffers :which-key "list")
+
+ ;; files
+ "f" '(:ignore t :which-key "files")
+ "ff" '(counsel-find-file :which-key "find file")
+ "fr" '(counsel-recentf :which-key "find recent")
+ "fs" '(save-buffer :which-key "save")
+
+ ;; git
+ "g" 'magit
+
+ ;; windows
+ "w" '(:ignore t :which-key "windows")
+ "wc" '(delete-window :which-key "close")
+ "wh" '(split-window-below :which-key "split horizontally")
+ "wj" '(delete-other-windows :which-key "delete others")
+ "wo" '(other-window :which-key "other")
+ "wv" '(split-window-right :which-key "split vertically")
+ )
+
 ;; =============================================================================
 ;; Auto generated code
 ;; =============================================================================
