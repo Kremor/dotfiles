5b3f374bb810d22f289673e1a2ba470ea74fb6fc
Kremor Pärt <kremor.part@gmail.com>
2019-01-29 13:09:13 -0600

    Adds more keybinding

--- .emacs.d/config/keys.el
+++ .emacs.d/config/keys.el
@@ -1,5 +1,9 @@
 (require 'general)
 
+;;remaps undo
+(general-define-key
+ "C-z" 'undo)
+
 ;; Ivy key bindings
 (general-define-key
  "C-s" 'swiper
@@ -46,20 +50,47 @@
  "bk" '(kill-this-buffer :which-key "kill")
  "bl" '(list-buffers :which-key "list")
 
+ ;; code
+ "c" '(:ignore t :which-key "code")
+ "cc" '(comment-or-uncomment-region :which-key "un/comment")
+ "cg" '(goto-line :which-key "go to line")
+
  ;; files
  "f" '(:ignore t :which-key "files")
  "ff" '(counsel-find-file :which-key "find file")
  "fr" '(counsel-recentf :which-key "find recent")
  "fs" '(save-buffer :which-key "save")
 
- ;; git
- "g" 'magit
+ ;; version control
+ "v" '(:ignore t :which-key "git")
+ "vb" '(:ignore t :which-key "branch")
+ "vbb" '(magit-branch-create :which-key "create")
+ "vbc" '(magit-branch-and-checkout :which "create anc checkout")
+ "vc" '(magit-checkout :which-key "checkout")
+ "vf" '(magit-fetch-from-upstream :which-key "fetch branch")
+ "vg" '(magit :which-key "magit")
+ "vp" '(magit-push-current-to-upstream :which-key "push upstream")
+ "vq" '(magit-pull-from-upstream :which-key "pull upstream")
 
  ;; windows
  "w" '(:ignore t :which-key "windows")
- "wc" '(delete-window :which-key "close")
+ "wd" '(delete-window :which-key "delete window")
+ "wf" '(:ignore t :which-key "frames")
+ "wfd" '(delete-frame :which-key "delete frame")
+ "wff" '(make-frame :which-key "make frame")
+ "wfj" '(delete-other-window :which-key "delete others")
+ "wfo" '(other-frame :which-key "other frame")
  "wh" '(split-window-below :which-key "split horizontally")
  "wj" '(delete-other-windows :which-key "delete others")
- "wo" '(other-window :which-key "other")
+ "wo" '(other-window :which-key "other window")
  "wv" '(split-window-right :which-key "split vertically")
+
+ ;; help
+ "?" '(:ignore t :which-key "help")
+ "?k" '(describe-key :which-key "describe key")
+ "?e" '(info-emacs-manual :which-key "emacs manual")
+ "?f" '(describe-function :which-key "describe function")
+ "?i" '(info :which-key "info")
+ "?v" '(describe-variable :which-key "describe variable")
+ "??" '(help :which-key "help")
  )
